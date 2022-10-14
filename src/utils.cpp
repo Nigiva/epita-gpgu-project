760e1eef1a46e662fd956a72daf529abf97f4068
Hao ye <hao.ye@epita.fr>
2022-10-14 18:22:02 +0200

    feat: add utils file with a function to generate gaussian kernel

---
+++ src/utils.cpp
@@ -0,0 +1,17 @@
+void gaussian_kernel(double *kernel, double sigma, int size){
+  double r, s = 2.0 * sigma * sigma;
+  double sum = 0.0;
+
+  int mid = size / 2;
+  for (int x = -mid; x <= mid; x++) {
+    for (int y = -mid; y <= mid; y++) {
+      r = sqrt(x * x + y * y);
+      kernel[(x + mid) * size + (y + mid)] = (exp(-(r * r) / s)) / (M_PI * s);
+      sum += kernel[(x + mid) * size + (y + mid)];
+    }
+  }
+  // normalising the Kernel
+  for (int i = 0; i < size; ++i)
+    for (int j = 0; j < size; ++j)
+      kernel[i * size + j] /= sum;
+}
---
+++ src/utils.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <cmath>
+
+void gaussian_kernel(double* kernel, double sigma = 1.0, int size);
